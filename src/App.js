503cc9e858410e3869fe765e922559d097cb5833
Mohd Shariq <155702117+mohdsha-123@users.noreply.github.com>
2024-07-31 10:18:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
+import Home from "./components/Home";
+import Navbar  from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
+import SocLinks from "./components/SocLinks";
+
+
+
+
+
+function App() {
+  return (
+    <div >
+     <Navbar />
+     <Home />
+     <About />
+     <Portfolio />
+     <Experience />
+     <Contact />
+     
+
+
+    <SocLinks />
+    </div>
+  );
+}
+
+export default App;
